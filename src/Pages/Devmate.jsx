97c9a7f47c386d10be2a258bd0eb559139730d80
shrirangop <shrirangelk221@gmail.com>
2024-01-17 23:05:36 +0530

    tailwind to bootstrap

--- src/Pages/Devmate.jsx
+++ src/Pages/Devmate.jsx
@@ -2,22 +2,24 @@ import mock_data from "../assets/MOCK_DATA.json";
 
 function Devmate() {
   return (
-    <div className=" overflow-x-hidden">
-      <div className="bg-[#FAFAFA] h-[80px] flex justify-between items-center m-auto px-4">
-        <h1 className="text-4xl">Devmate</h1>
-        <div>
+    <div className="overflow-x-hidden">
+      <div className="d-flex justify-content-around">
+        <h1 className="p-2 flex-fill">Devmate</h1>
+        <div className = "p-2 flex-fill">
           <input type="search" id="search" placeholder="Search Projects" />
         </div>
-        <button className="bg-[#020049] text-white font-semibold py-3 px-2 rounded-xl">Create Project +</button>
+        <button className="p-2 flex-fill btn btn-primary">Create Project +</button>
       </div>
-      <div className="h-auto w-screen flex flex-wrap justify-center items-center gap-10 mt-8 bg-[#FAFAFA]">
+      <div className="container">
         {mock_data.map((item)=>{
           return (
-            <div key={item.project_id} className=" h-[500px] max-h-[500px] w-1/4 overflow-hidden border-2 border-[#060b11] border-solid rounded-xl relative">
-              <img src={item.thumbnail} alt="" className=" w-full object-contain absolute z-10 top-0"/>
-              <section className="absolute bottom-0 h-[40%] bg-[#dfd2e1] px-4 py-2 z-20 overflow-hidden">
-                <h1 className=" text-2xl mb-2">{item.project_name}</h1>
-                <p className="">{item.project_description}</p>
+            <div className="col">
+             {/* <div className="col"> */}
+            <div key={item.project_id} className="card" style = {{width:'33%'}}>
+              <img src={item.thumbnail} alt="" className="card-img-top"/>
+              <section className="card-body">
+                <h1 className="card-title">{item.project_name}</h1>
+                <p className="card-text">{item.project_description}</p>
                 <section className="h-[50%] bg-black text-white w-full">
                   {item.tags.map((tag)=>{
                     return (
@@ -27,9 +29,12 @@ function Devmate() {
                 </section>
               </section>
             </div>
+            {/* </div>  */}
+            </div>
           )
         })}
       </div>
+      
       <div className="h-[100px] w-full bg-[#FAFAFA]"></div>
     </div>
   )
